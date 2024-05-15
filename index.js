e2f9f8ee26eb5bdf1841f4c7fe86c92d25ba37ca
ayshakanta <ayshahussain291@gmail.com>
2024-05-15 14:43:57 +0600

    loaded room details data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 
@@ -30,6 +30,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const roomCollection = client.db('gringotts').collection('rooms')
+
+
+    app.get('/rooms', async(req, res)=>{
+        const cursor = roomCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/rooms/:id', async(req, res)=>{
+        const id = req.params.id 
+        const query = {_id: new ObjectId(id)}
+        const result = await roomCollection.findOne(query)
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
